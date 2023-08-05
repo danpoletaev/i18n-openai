2d4a631b1e9e0f9404879e44c6d4a5c87a78ff06
Daniil Poletaev <danpoletaev1@gmail.com>
2023-08-05 22:10:48 +0200

    🚚 chore: added locale to languages map

---
+++ src/resources/languages.ts
@@ -0,0 +1,23 @@
+import { Locales } from '../interface';
+
+export const localeToLanguageMap: { [key in Locales]: string } = {
+  ar: 'Arabic',
+  cs: 'Czech',
+  de: 'German',
+  es: 'Spanish',
+  fr: 'French',
+  id: 'Indonesian',
+  it: 'Italian',
+  ja: 'Japanese',
+  ko: 'Korean',
+  nl: 'Dutch',
+  pl: 'Polish',
+  pt: 'Portuguese',
+  ru: 'Russian',
+  sk: 'Slovak',
+  th: 'Thai',
+  tr: 'Turkish',
+  uk: 'Ukrainian',
+  vi: 'Vietnamese',
+  zh: 'Chinese',
+};
