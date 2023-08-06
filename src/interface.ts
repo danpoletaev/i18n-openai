504439813f9763b72965e0e406416de745c228ad
Daniil Poletaev <danpoletaev1@gmail.com>
2023-08-06 15:38:34 +0200

    🏷️ chore: updated types

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,5 @@
+import { getPath } from './utils/path';
+
 export type ObjectType = { [key: string]: string | any[] | any };
 
 export type ObjectArrayOrString = ObjectType | string | any[];
@@ -30,4 +32,24 @@ export interface IConfig {
   skipLocales: string[];
   mainLocale: Locales;
   pathToLocalesFolders: string;
+}
+
+export interface IRuntimePaths {
+  LOCALES_FOLDER: string;
+  MAIN_LOCALE_FOLDER: string;
+}
+
+export interface IParsedArguments {
+  locales?: string[];
+  files?: string[];
+}
+
+export interface IFiltered {
+  locales: string[];
+  files: string[];
+}
+
+export interface IConfigWithPaths {
+  config: IConfig;
+  runtimePaths: IRuntimePaths;
 }
