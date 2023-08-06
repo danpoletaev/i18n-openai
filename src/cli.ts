70caa62dd85b541a40e8cfe98975542700590145
Daniil Poletaev <danpoletaev1@gmail.com>
2023-08-06 17:48:57 +0200

    ✨feat: added main logic to cli class

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,22 @@
 import { Logger } from './logger';
 import { ConfigParser } from './parsers/config-parser';
 import { ArgumentsParser } from './parsers/arguments-parser';
+import { FileProcessor } from './file-processor';
+import { IFiltered, ProcessFunction } from './interface';
+import { Translator } from './translator';
 const figlet = require('figlet');
 
 /**
  * Class used to handle main program flow
  */
 export class CLI {
+  private processFn: ProcessFunction = (_str: string) =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('translated');
+      }, 10);
+    });
+
   /**
    * Main method
    */
@@ -15,7 +25,16 @@ export class CLI {
     const argumentParser = new ArgumentsParser();
 
     const configParser = new ConfigParser();
-    await configParser.loadConfig();
+
+    const config = configParser.loadConfig();
+
+    const parsedArguments = argumentParser.getArguments();
+
+    const fileProcessor = new FileProcessor(config, parsedArguments);
+    const filtered: IFiltered = await fileProcessor.getFiltered();
+
+    const translator = new Translator(filtered, config.runtimePaths, this.processFn);
+    await translator.translate();
   }
 
   /**
