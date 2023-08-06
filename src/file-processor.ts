6d7b56b8ed2e333ff8c7c37613e941d1063a6747
Daniil Poletaev <danpoletaev1@gmail.com>
2023-08-06 15:36:24 +0200

    ✨feat: added file processor class with tests

---
+++ src/file-processor.ts
@@ -0,0 +1,73 @@
+import { Logger } from './logger';
+import fs from 'fs';
+import { IConfigWithPaths, IFiltered, IParsedArguments } from './interface';
+
+/**
+ * Class, that reads files from filesystem and filters it
+ */
+export class FileProcessor {
+  private readonly _config: IConfigWithPaths;
+  private readonly _arguments: IParsedArguments;
+
+  constructor(config: IConfigWithPaths, parsedArguments: IParsedArguments) {
+    this._config = config;
+    this._arguments = parsedArguments;
+  }
+
+  /**
+   * Gets filtered locale array. If the array of locales is empty throws and error
+   */
+  private getFilteredLocales() {
+    const localesFolder = this._config.runtimePaths.LOCALES_FOLDER;
+    const allLocalesInFolder = fs.readdirSync(localesFolder);
+
+    const localesArg = this._arguments.locales;
+
+    const filteredAllLocales = allLocalesInFolder
+      .filter(
+        (locale) => !this._config.config.skipLocales.includes(locale) && locale !== this._config.config.mainLocale,
+      )
+      .filter((locale) => (!!localesArg ? localesArg.includes(locale) : true));
+
+    if (filteredAllLocales.length === 0) {
+      Logger.incorrectFilteredLocales(localesArg?.join(',') ?? '', allLocalesInFolder.join(','));
+      throw new Error();
+    }
+
+    return filteredAllLocales;
+  }
+
+  /**
+   * Gets filtered files array. If the array of files is empty throws and error
+   */
+  private getFilteredFiles() {
+    const mainLocaleFolder = this._config.runtimePaths.MAIN_LOCALE_FOLDER;
+    const allFilesInMainLocaleFolder = fs.readdirSync(mainLocaleFolder);
+
+    const filesArg = this._arguments.files;
+
+    const filteredAllFilesInMainLocaleFolder = allFilesInMainLocaleFolder.filter((file) =>
+      !!filesArg ? filesArg.includes(file) : true,
+    );
+    if (filteredAllFilesInMainLocaleFolder.length === 0) {
+      Logger.incorrectFilteredFiles(filesArg?.join(',') ?? '', allFilesInMainLocaleFolder.join(','));
+      throw new Error();
+    }
+
+    return filteredAllFilesInMainLocaleFolder;
+  }
+
+  /**
+   * Gets filtered files and locales array
+   */
+  getFiltered(): IFiltered {
+    const locales = this.getFilteredLocales();
+
+    const files = this.getFilteredFiles();
+
+    return {
+      locales,
+      files,
+    };
+  }
+}
---
+++ test/file-processor.spec.ts
@@ -0,0 +1,130 @@
+import { IConfigWithPaths, IParsedArguments } from '../src';
+import { assert, expect } from 'chai';
+import { FileProcessor } from '../src/file-processor';
+// @ts-ignore
+import mock from 'mock-fs';
+
+describe('getFiltered', () => {
+  beforeEach(() => {
+    mock({
+      'public/locales': {
+        en: {
+          'test.json': '',
+          'test2.json': '',
+          'test3.json': '',
+        },
+        cs: {},
+        de: {},
+        es: {},
+      },
+    });
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
+  const config: IConfigWithPaths = {
+    config: {
+      skipLocales: ['default'],
+      pathToLocalesFolders: 'public/locales',
+      mainLocale: 'en',
+    },
+    runtimePaths: {
+      LOCALES_FOLDER: 'public/locales',
+      MAIN_LOCALE_FOLDER: 'public/locales/en',
+    },
+  };
+
+  context('when args are undefined', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      locales: undefined,
+      files: undefined,
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      const filtered = fileProcessor.getFiltered();
+      assert.equal(JSON.stringify(['cs', 'de', 'es']), JSON.stringify(filtered.locales));
+    });
+  });
+
+  context('when locale is selected', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      locales: ['es'],
+      files: undefined,
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      const filtered = fileProcessor.getFiltered();
+      assert.equal(JSON.stringify(args.locales), JSON.stringify(filtered.locales));
+    });
+  });
+
+  context('when filtered locales are empty', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      locales: ['ar'],
+      files: undefined,
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      expect(() => fileProcessor.getFiltered()).to.throw();
+    });
+  });
+
+  context('when file arg is not provided', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      locales: undefined,
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      const filtered = fileProcessor.getFiltered();
+      assert.equal(JSON.stringify(['test.json', 'test2.json', 'test3.json']), JSON.stringify(filtered.files));
+    });
+  });
+
+  context('when file arg is provided', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      locales: undefined,
+      files: ['test.json', 'test2.json'],
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      const filtered = fileProcessor.getFiltered();
+      assert.equal(JSON.stringify(args.files), JSON.stringify(filtered.files));
+    });
+  });
+
+  context('when filtered files are empty', () => {
+    let fileProcessor: FileProcessor;
+    const args: IParsedArguments = {
+      files: ['noexists.json'],
+    };
+
+    before(() => {
+      fileProcessor = new FileProcessor(config, args);
+    });
+    it('should be equal', () => {
+      expect(() => fileProcessor.getFiltered()).to.throw();
+    });
+  });
+});
